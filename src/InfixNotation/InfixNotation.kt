1a36286b1efd558edc41c9c6216074368ad7c667
Doaa Mosalam <dodoo_mosalem@yahoo.com>
2023-04-28 11:35:30 +0300

    adding new lesson

--- src/InfixNotation/InfixNotation.kt
+++ src/InfixNotation/InfixNotation.kt
@@ -3,6 +3,14 @@ package InfixNotation
 fun main(args: Array<String>) {
     val sum = 5 add 5 //infix call
     println(sum)
+    //===========================================================
+    val mul = 5 multiplay 5
+    println(mul)
+    //=========================================================
+    val info = "Doaa Mosalam" printInfo  " Software Engineer"
+    println(info)
 }
 
 infix fun Int.add(value:Int):Int = this + value
+infix fun Int.multiplay(value: Int) = this * value
+infix fun String.printInfo(person:String):String = this + person
